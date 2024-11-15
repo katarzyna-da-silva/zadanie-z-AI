b9668e2639461fab2d65509c319a91783c640a25
katarzyna-da-silva <conic916@gmail.com>
2024-11-15 20:59:39 +0100

    add code

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const fs = require('fs');
 const OpenAI = require('openai');
-
-META_PROMPT = `
-`.strip();
-
+require('dotenv').config();
+console.log(process.env.OPENAI_API_KEY);
 // Wczytywanie pliku 
-fs.readFile('/home/z390/Bureau/zadanie/artykul.txt', 'utf8', async (err, data) => {
+fs.readFile('/home/z390/Bureau/zadanie/text.txt', 'utf8', async (err, data) => {
   if (err) {
     console.error("Błąd wczytywania pliku:", err);
     return;
@@ -21,19 +19,31 @@ fs.readFile('/home/z390/Bureau/zadanie/artykul.txt', 'utf8', async (err, data) =
         messages: [
           {
             role: "user",
-            content: META_PROMPT,
-          }
+            content: `Please convert the following article into HTML code. It should include headings, paragraphs, and proper HTML structure:
+                    Article title: "Understanding the Basics of HTML"
+                    Content: "${data}"
+                    
+                    Use h1 to h5 headers, choose appropriately according to html and css rules.
+
+                    Use <img> tags with src="image_placeholder.jpg" attribute. Add alt attribute to each image with exact prompt that we can use to generate the graphic.
+
+                    Place captions under the graphics using appropriate HTML tag.
+
+                    No CSS or JavaScript code. The returned code should contain only content to be inserted between <body> and </body> tags.
+
+                    Do not include <html>, <head> or <body> tags. `
+        }
         ]
       });
 
         const outputHtml = response.choices[0].message.content;
 
 
-fs.writeFile('/home/z390/Bureau/zadanie/output.html', outputHtml, (err) => {
+fs.writeFile('/home/z390/Bureau/zadanie/artykul.html', outputHtml, (err) => {
 if (err) {
   console.log('Sprobuj ponownie.Problem z plikiem', err);
 } else {
-  console.log('Plik zapisany w output.html');
+  console.log('Plik zapisany w artykul.html');
 }
 });
       }catch (apiErr) {
